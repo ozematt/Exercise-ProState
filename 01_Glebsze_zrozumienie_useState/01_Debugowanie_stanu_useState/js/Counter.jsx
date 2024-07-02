0472b862e0818b0ff620052caed736e77949b424
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-02 12:57:54 +0200

    fixed exercise

--- 01_Glebsze_zrozumienie_useState/01_Debugowanie_stanu_useState/js/Counter.jsx
+++ 01_Glebsze_zrozumienie_useState/01_Debugowanie_stanu_useState/js/Counter.jsx
@@ -1,40 +1,36 @@
 import React, { useState, useEffect } from 'react';
 
 export const Counter = () => {
-  let [count, setCount] = useState(0);
+  ////DATA
+  const [count, setCount] = useState(0);
 
+  ////LOGIC
   const fetchData = async () => {
     const data = await new Promise((resolve) => setTimeout(() => resolve(10), 2000));
-    count += data;
+    setCount((prevState) => prevState + data);
   };
 
   useEffect(() => {
     console.log('Counter has changed:', count);
-  });
+  }, [count]);
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const handleDecrease = () => {
+    setCount((prevState) => prevState + 1);
+  };
+  const handleIncrease = () => {
+    setCount((prevState) => prevState - 1);
+  };
+
+  ////UI
   return (
     <div>
       <p>Counter: {count}</p>
-      <button
-        onClick={() => {
-          count++;
-          setCount(count);
-        }}
-      >
-        Increase
-      </button>
-      <button
-        onClick={() => {
-          count--;
-          setCount(count);
-        }}
-      >
-        Decrease
-      </button>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
     </div>
   );
 };
