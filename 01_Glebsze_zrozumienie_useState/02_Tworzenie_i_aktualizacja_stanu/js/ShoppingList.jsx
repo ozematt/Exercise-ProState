72b5a2a4a3db02ebd20739be9be55f88da74424d
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-02 13:07:44 +0200

    added logic in ShoppingApp

--- 01_Glebsze_zrozumienie_useState/02_Tworzenie_i_aktualizacja_stanu/js/ShoppingList.jsx
+++ 01_Glebsze_zrozumienie_useState/02_Tworzenie_i_aktualizacja_stanu/js/ShoppingList.jsx
@@ -1,3 +1,41 @@
+import { useState } from 'react';
+
 export const ShoppingList = () => {
-  return null;
+  const [products, setProducts] = useState([]);
+  const [item, setItem] = useState('');
+  const [error, setError] = useState('');
+
+  const handleProductAdd = (e) => {
+    e.preventDefault();
+    if (item) {
+      setProducts((prevState) => [...prevState, { name: item, id: Date.now() }]);
+      setItem('');
+      setError('');
+    } else {
+      setError('Pole nie może być puste');
+    }
+  };
+  const handleProductRemove = (id) => {
+    setProducts((prevState) => prevState.filter((item) => item.id !== id));
+  };
+  return (
+    <>
+      <form onSubmit={handleProductAdd}>
+        <input value={item} type="text" onChange={(e) => setItem(e.target.value)} />
+        <button type="submit">Dodaj</button>
+        <p style={{ color: 'red' }}>{error}</p>
+      </form>
+      <div>
+        <h2>Lista Produktów</h2>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.name}
+              <button onClick={() => handleProductRemove(product.id)}>Usuń produkt</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 /**
  * Path to exercise folder
  */
-const exercisePath = '01_Glebsze_zrozumienie_useState/01_Debugowanie_stanu_useState';
+const exercisePath = '01_Glebsze_zrozumienie_useState/02_Tworzenie_i_aktualizacja_stanu';
 
 /**
  * Don't change those lines below
