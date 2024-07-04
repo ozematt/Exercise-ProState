9188f7d5352bb7b9bf214ba91236fd051136c75a
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-04 12:30:55 +0200

    added filtered

--- 04_Optmalizacja_wydajnosci_z_useState_i_useReducer/02_Tworzenie_wydajnego_komponentu/js/UserManagementPanel.jsx
+++ 04_Optmalizacja_wydajnosci_z_useState_i_useReducer/02_Tworzenie_wydajnego_komponentu/js/UserManagementPanel.jsx
@@ -1,22 +1,32 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 export const UserManagementPanel = () => {
   const [comment, setComment] = useState('');
-  const [list, setList] = useState([]);
+  const [commentsList, setCommentsList] = useState([]);
+  const [filter, setFilter] = useState('');
+
   console.log(comment);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setList((prevState) => [...prevState, comment]);
+    setCommentsList((prevState) => [...prevState, comment]);
   };
+
+  const filteredComments = useMemo(
+    () => commentsList.filter((comment) => comment.toLowerCase().includes(filter.toLowerCase())),
+    [filter]
+  );
 
   return (
     <>
       <form onSubmit={handleSubmit}>
         <input value={comment} type="text" onChange={(e) => setComment(e.target.value)} />
         <button type="submit">Dodaj</button>
+        <br />
+        <input value={filter} type="text" onChange={(e) => setFilter(e.target.value)} placeholder="Filter.." />
+
         <ul>
-          {list.map((item, index) => (
+          {commentsList.map((item, index) => (
             <li key={index}>{item}</li>
           ))}
         </ul>
