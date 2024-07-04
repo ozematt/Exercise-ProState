37997e28c2a1fa3df377716daf97dd73cec13d55
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-04 12:54:32 +0200

    added logic

--- 04_Optmalizacja_wydajnosci_z_useState_i_useReducer/02_Tworzenie_wydajnego_komponentu/js/CommentForm.jsx
+++ 04_Optmalizacja_wydajnosci_z_useState_i_useReducer/02_Tworzenie_wydajnego_komponentu/js/CommentForm.jsx
@@ -3,6 +3,10 @@ import { UserManagementPanel } from './UserManagementPanel.jsx';
 export const CommentForm = () => {
   return (
     <>
+      <div style={{ border: '1px solid black', borderRadius: '10px', padding: '1rem' }}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi cum dolore ea harum in incidunt ipsa labore
+        libero magnam numquam quaerat quam qui quis quo, recusandae sunt, vero voluptate voluptates!
+      </div>
       <UserManagementPanel />
     </>
   );
--- 04_Optmalizacja_wydajnosci_z_useState_i_useReducer/02_Tworzenie_wydajnego_komponentu/js/UserManagementPanel.jsx
+++ 04_Optmalizacja_wydajnosci_z_useState_i_useReducer/02_Tworzenie_wydajnego_komponentu/js/UserManagementPanel.jsx
@@ -1,30 +1,55 @@
-import { useMemo, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 export const UserManagementPanel = () => {
   const [comment, setComment] = useState('');
   const [commentsList, setCommentsList] = useState([]);
   const [filter, setFilter] = useState('');
 
-  console.log(filter);
-  console.log(commentsList);
   const handleSubmit = (e) => {
     e.preventDefault();
     setCommentsList((prevState) => [...prevState, comment]);
     setComment('');
   };
 
-  const filteredComments = () => commentsList.filter((comment) => comment.toLowerCase().includes(filter.toLowerCase()));
-  console.log(filteredComments());
+  const filteredComments = useMemo(
+    () => commentsList.filter((comment) => comment.toLowerCase().includes(filter.toLowerCase())),
+    [filter, commentsList]
+  );
+  console.log(commentsList);
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input value={comment} type="text" onChange={(e) => setComment(e.target.value)} />
-        <button type="submit">Dodaj</button>
+      <br />
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
         <br />
-        <input value={filter} type="text" onChange={(e) => setFilter(e.target.value)} placeholder="Filter.." />
-
-        <ul>{!filter ? commentsList.map((item, index) => <li key={index}>{item}</li>) : filteredComments()}</ul>
+        <input
+          style={{
+            width: '500px',
+            height: '50px',
+          }}
+          value={comment}
+          type="text"
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <button
+          style={{
+            padding: '12px',
+          }}
+          type="submit"
+        >
+          Dodaj
+        </button>
       </form>
+      <br />
+      <input value={filter} type="text" onChange={(e) => setFilter(e.target.value)} placeholder="Filter.." />
+
+      <ul>{!filter ? commentsList.map((item, index) => <li key={index}>{item}</li>) : filteredComments}</ul>
     </>
   );
 };
