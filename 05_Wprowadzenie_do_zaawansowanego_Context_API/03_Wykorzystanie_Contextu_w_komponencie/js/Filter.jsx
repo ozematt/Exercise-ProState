457068ff54bb59d70b5cb52dc383b9cdcc0af582
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-08 16:48:38 +0200

    added comments

--- 05_Wprowadzenie_do_zaawansowanego_Context_API/03_Wykorzystanie_Contextu_w_komponencie/js/Filter.jsx
+++ 05_Wprowadzenie_do_zaawansowanego_Context_API/03_Wykorzystanie_Contextu_w_komponencie/js/Filter.jsx
@@ -2,8 +2,9 @@ import { useContext } from 'react';
 import { TasksContext } from './TasksContext.jsx';
 
 export const Filter = () => {
+  ////DATA
   const { handleTasksFilter } = useContext(TasksContext);
-
+  ////UI
   return (
     <>
       <select name="filter" id="filter" onChange={(e) => handleTasksFilter(e.target.value)}>
