5f0cb8258262a9b166761e9076dbc570f51c09b1
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-09 13:27:53 +0200

    added reducer

--- 06_Integracja_useReducer_z_Context_API/01_Tworzenie_wlasnego_stanu_globalnego/js/UserPreferencesContext.jsx
+++ 06_Integracja_useReducer_z_Context_API/01_Tworzenie_wlasnego_stanu_globalnego/js/UserPreferencesContext.jsx
@@ -4,6 +4,33 @@ const UserPreferencesContext = createContext();
 
 const initialState = { theme: 'light', language: 'en', notifications: true };
 
+const ActionsTypes = {
+  SET_THEME: 'SET_THEME',
+  SET_LANGUAGE: 'SET_LANGUAGE',
+  TOGGLE_NOTIFICATION: 'TOGGLE_NOTIFICATION',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ActionsTypes.SET_THEME{
+      return { ...state, theme: action.theme };
+    }
+    case ActionsTypes.SET_LANGUAGE{
+      return { ...state, language: action.language };
+    }
+    case ActionsTypes.TOGGLE_NOTIFICATION: {
+      return { ...state, notifications: action.notification };
+    }
+  }
+};
+
 const UserPreferencesProvider = ({ children }) => {
-  const [state, dispatch] = useReducer();
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return(
+    <>
+    <UserPreferencesContext.Provider value={{ state, dispatch }}>
+      {children}
+    </UserPreferencesContext.Provider>
+    </>
+  )
 };
