95530688ed58bc2be890721541d5c08c8168b72b
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-15 17:19:57 +0200

    aded axios fetch data

--- 11_Wprowadzenie_do_TanStack_Query/01_Implementacja_useQuery_w_praktyce/js/PostsList.jsx
+++ 11_Wprowadzenie_do_TanStack_Query/01_Implementacja_useQuery_w_praktyce/js/PostsList.jsx
@@ -1,3 +1,10 @@
+import axios from 'axios';
+
+const fetchData = async () => {
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return data;
+};
+
 export const PostsList = () => {
   return null;
 };
