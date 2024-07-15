2e30a74232bce75bed0e630f87a32359cbc84e06
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-15 17:46:34 +0200

    solved exercise

--- 11_Wprowadzenie_do_TanStack_Query/01_Implementacja_useQuery_w_praktyce/js/PostsList.jsx
+++ 11_Wprowadzenie_do_TanStack_Query/01_Implementacja_useQuery_w_praktyce/js/PostsList.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 
 const fetchData = async () => {
   const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
@@ -6,5 +7,20 @@ const fetchData = async () => {
 };
 
 export const PostsList = () => {
-  return null;
+  const { isPending, data, isError, error } = useQuery({ queryKey: ['posts'], queryFn: fetchData });
+  console.log(data);
+  return (
+    <>
+      {isPending && <div>Loading...</div>}
+      {isError && <div>{error.message}</div>}
+      <ol>
+        {data.map((post) => (
+          <li key={post.id}>
+            <h6>{post.title}</h6>
+            <p>{post.body.slice(0, 100)}...</p>
+          </li>
+        ))}
+      </ol>
+    </>
+  );
 };
