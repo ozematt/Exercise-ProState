3304460866a33f9f27f8808384338b6f75ec90db
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-16 12:43:06 +0200

    added logic

--- 11_Wprowadzenie_do_TanStack_Query/04_Rozbudowa_aplikacji_z_uzyciem_TanStack_Query/js/OrdersList.jsx
+++ 11_Wprowadzenie_do_TanStack_Query/04_Rozbudowa_aplikacji_z_uzyciem_TanStack_Query/js/OrdersList.jsx
@@ -1,33 +1,38 @@
 import { Typography, Card, CardContent, List, ListItem, ListItemText, Button } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
 
 export const OrdersList = () => {
-  const orders = [];
+  const { data } = useQuery({
+    queryKey: ['orders'],
+    queryFn: () => fetch('http://localhost:3001/orders').then((res) => res.json()),
+  });
 
   return (
     <div>
       <Typography variant="h5">Orders List</Typography>
-      {orders.map((order) => (
-        <Card key={order.id} elevation={0}>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {order.firstName} {order.lastName}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {order.address}
-            </Typography>
-            <List>
-              {order.products.map((product) => (
-                <ListItem key={product.id}>
-                  <ListItemText primary={product.name} secondary={product.price} />
-                </ListItem>
-              ))}
-            </List>
-            <Button variant="contained" color="error">
-              Delete
-            </Button>
-          </CardContent>
-        </Card>
-      ))}
+      {data &&
+        data.map((order) => (
+          <Card key={order.id} elevation={0}>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {order.firstName} {order.lastName}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {order.address}
+              </Typography>
+              <List>
+                {order.cart.map((product) => (
+                  <ListItem key={product.id}>
+                    <ListItemText primary={product.name} secondary={product.price} />
+                  </ListItem>
+                ))}
+              </List>
+              <Button variant="contained" color="error">
+                Delete
+              </Button>
+            </CardContent>
+          </Card>
+        ))}
     </div>
   );
 };
