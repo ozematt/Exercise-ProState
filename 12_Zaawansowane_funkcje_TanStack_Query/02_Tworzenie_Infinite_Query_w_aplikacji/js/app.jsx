f7bb3a8ae78da8a4f5d6df4f9cc77ca13a4f1fc0
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-17 11:56:13 +0200

    added queryClient

--- 12_Zaawansowane_funkcje_TanStack_Query/02_Tworzenie_Infinite_Query_w_aplikacji/js/app.jsx
+++ 12_Zaawansowane_funkcje_TanStack_Query/02_Tworzenie_Infinite_Query_w_aplikacji/js/app.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { PhotosGrid } from './PhotosGrid';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const App = () => <PhotosGrid />;
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <PhotosGrid />
+  </QueryClientProvider>
+);
 
 const container = document.getElementById('app');
 const root = createRoot(container);
