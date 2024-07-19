5fc8a064c38bcd4cc20bd7e7a45474ea3c3af990
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-19 11:07:25 +0200

    edided routes

--- 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/app.jsx
+++ 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/app.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { StepOne } from './StepOne.jsx';
 import { StepTwo } from './StepTwo.jsx';
 import { StepThree } from './StepThree.jsx';
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/stepone" element={StepOne} />
-        <Route path="/steptwo" element={StepTwo} />
-        <Route path="/stepthree" element={StepThree} />
+        <Route path="/" element={<StepOne />} />
+        <Route path="/steptwo" element={<StepTwo />} />
+        <Route path="/stepthree" element={<StepThree />} />
       </Routes>
     </BrowserRouter>
   );
