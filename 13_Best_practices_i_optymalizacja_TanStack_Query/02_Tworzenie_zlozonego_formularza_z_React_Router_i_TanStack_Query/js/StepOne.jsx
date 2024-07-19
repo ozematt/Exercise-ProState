1f25f29f55bd59b9982ecd2548d728bb0545cf14
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-19 11:16:27 +0200

    added step one ui

--- 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/StepOne.jsx
+++ 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/StepOne.jsx
@@ -1,3 +1,16 @@
+import { useForm } from 'react-hook-form';
+
 export const StepOne = () => {
-  return null;
+  const { register } = useForm();
+
+  return (
+    <>
+      <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+        <input type="text" placeholder="Imię" />
+        <input type="text" placeholder="Nazwisko" />
+        <input type="email" placeholder="Email" />
+        <button>Dalej</button>
+      </form>
+    </>
+  );
 };
