db7c1ee090145eacbd012f43b17b9c38dc600426
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-19 11:32:07 +0200

    added register in step two

--- 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/StepTwo.jsx
+++ 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/StepTwo.jsx
@@ -1,14 +1,17 @@
 import { Box, TextField } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 export const StepTwo = () => {
+  const { register } = useForm();
+
   return (
     <>
       <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-        <TextField type="text" placeholder="Ulica" />
-        <TextField type="text" placeholder="Numerdomu/mieszkania" />
-        <TextField type="text" placeholder="Miasto" />
-        <TextField type="text" placeholder="Kod pocztowy" />
+        <TextField {...register('street')} type="text" placeholder="Ulica" />
+        <TextField {...register('houseNumber')} type="text" placeholder="Numerdomu/mieszkania" />
+        <TextField {...register('city')} type="text" placeholder="Miasto" />
+        <TextField {...register('postalCode')} type="text" placeholder="Kod pocztowy" />
         <Box>
           <Link style={{ padding: '10px' }} to="/steptwo">
             Wstecz
