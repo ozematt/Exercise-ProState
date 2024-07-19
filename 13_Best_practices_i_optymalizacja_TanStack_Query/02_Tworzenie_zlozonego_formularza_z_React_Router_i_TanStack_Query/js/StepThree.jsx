9590fb6886a260b6bc3c541bcec56a6f61dcc91e
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-19 12:57:34 +0200

    added summary

--- 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/StepThree.jsx
+++ 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/StepThree.jsx
@@ -1,14 +1,52 @@
 import { Button } from '@mui/material';
 import { useFormContext } from './FormContext.jsx';
+import { Link } from 'react-router-dom';
 
 export const StepThree = () => {
   const { state, handleSubmit } = useFormContext();
   return (
     <>
       <h4>Podsumowanie i potwierdzenie:</h4>
-      <div>Dane Osobowe:</div>
-      <div>Adres:</div>
-      <Button>Edytuj</Button>
+      <div>
+        <h5>Dane Osobowe:</h5>
+        <p>
+          <b>Imię: </b>
+          {state.name}
+        </p>
+        <p>
+          <b>Nazwisko: </b>
+          {state.surname}
+        </p>
+        <p>
+          <b>Email: </b>
+          {state.email}
+        </p>
+        <Link to="/stepone">
+          <Button>Edytuj</Button>
+        </Link>
+      </div>
+      <div>
+        <h5>Adres:</h5>
+        <p>
+          <b>Ulica: </b>
+          {state.street}
+        </p>
+        <p>
+          <b>Numer domu: </b>
+          {state.houseNumber}
+        </p>
+        <p>
+          <b>Miasto: </b>
+          {state.city}
+        </p>
+        <p>
+          <b>Kod pocztowy: </b>
+          {state.postalCode}
+        </p>
+        <Link to="/steptwo">
+          <Button>Edytuj</Button>
+        </Link>
+      </div>
       <Button>Wyślij</Button>
     </>
   );
