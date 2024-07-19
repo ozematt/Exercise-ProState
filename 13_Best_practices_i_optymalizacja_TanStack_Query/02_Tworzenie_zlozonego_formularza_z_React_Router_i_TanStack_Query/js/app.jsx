6b3d8c1193bc7eea0d7539c09a2afaa23030eac9
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-19 11:04:26 +0200

    added Router

--- 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/app.jsx
+++ 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/app.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-const App = () => null;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { StepOne } from './StepOne.jsx';
+import { StepTwo } from './StepTwo.jsx';
+import { StepThree } from './StepThree.jsx';
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/stepone" element={StepOne} />
+        <Route path="/steptwo" element={StepTwo} />
+        <Route path="/stepthree" element={StepThree} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 const container = document.getElementById('app');
 const root = createRoot(container);
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
  * Path to exercise folder
  */
 const exercisePath =
-  '13_Best_practices_i_optymalizacja_TanStack_Query/01_Praktyczne_zastosowanie_Best_Practices_TanStack_Query';
+  '13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query';
 
 /**
  * Don't change those lines below
