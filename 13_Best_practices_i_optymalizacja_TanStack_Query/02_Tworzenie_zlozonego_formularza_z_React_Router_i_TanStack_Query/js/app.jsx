203b11d8a72f520ac65eb7f20e6f9c7605ef23b6
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-20 10:51:24 +0200

    added Routes

--- 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/app.jsx
+++ 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/app.jsx
@@ -4,23 +4,24 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { StepOne } from './StepOne.jsx';
 import { StepTwo } from './StepTwo.jsx';
 import { StepThree } from './StepThree.jsx';
-import { FormContextProvider } from './FormContext.jsx';
+
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { FormContext } from './FormContext.jsx';
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <FormContextProvider>
-        <BrowserRouter>
+      <BrowserRouter>
+        <FormContext>
           <Routes>
             <Route path="/" element={<StepOne />} />
             <Route path="/steptwo" element={<StepTwo />} />
             <Route path="/stepthree" element={<StepThree />} />
           </Routes>
-        </BrowserRouter>
-      </FormContextProvider>
+        </FormContext>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 };
