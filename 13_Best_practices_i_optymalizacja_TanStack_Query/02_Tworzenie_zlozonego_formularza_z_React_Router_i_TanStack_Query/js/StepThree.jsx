3ab02aad2e1a1b328c7929b143d00442b071a240
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-24 11:52:04 +0200

    added comments in StepThree

--- 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/StepThree.jsx
+++ 13_Best_practices_i_optymalizacja_TanStack_Query/02_Tworzenie_zlozonego_formularza_z_React_Router_i_TanStack_Query/js/StepThree.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 import { useFormContext } from './FormContext';
 
+//handle post data on JSON sever
 const sendFormData = async (formData) => {
   const response = await fetch('http://localhost:3001/formData', {
     method: 'POST',
@@ -19,10 +20,14 @@ const sendFormData = async (formData) => {
 };
 
 export const StepThree = () => {
+  //DATA
+  //navigate
   const navigate = useNavigate();
+  //data mutation
   const mutation = useMutation({ mutationFn: sendFormData });
+  //use Context
   const { clearData, data } = useFormContext();
-
+  //mutate data form state context, when ok -> clear form, show alert and navigate
   const handleSubmit = () => {
     mutation.mutate(data, {
       onSuccess: () => {
@@ -32,10 +37,10 @@ export const StepThree = () => {
       },
     });
   };
-
+  //validations on different state
   if (mutation.isLoading) return <div>Submitting...</div>;
   if (mutation.isError) return <div>Error: {mutation.error.message}</div>;
-
+  ////UI
   return (
     <div>
       <h2>Podsumowanie</h2>
@@ -46,6 +51,10 @@ export const StepThree = () => {
           </div>
         ))}
       </div>
+      {/*Object.entries(data): Konwertuje obiekt data na tablicę par [klucz, wartość].
+      Każdy element tej tablicy jest również tablicą zawierającą dwa elementy: klucz i wartość.
+      .map(([key, value]) => (...)): Iteruje przez każdą parę [key, value] z tablicy wynikowej Object.entries(data).
+       Dla każdej pary wykonuje funkcję strzałkową ([key, value]) => (...).*/}
       <button type="button" onClick={() => navigate('/personal-info')}>
         Wstecz
       </button>
