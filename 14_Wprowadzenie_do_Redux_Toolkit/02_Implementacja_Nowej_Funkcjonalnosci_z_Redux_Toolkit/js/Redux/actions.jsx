67f4ab6aeeffe0eb50f931997ad6799392d45dbf
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-25 11:49:00 +0200

    addeedd

--- 14_Wprowadzenie_do_Redux_Toolkit/02_Implementacja_Nowej_Funkcjonalnosci_z_Redux_Toolkit/js/Redux/actions.jsx
+++ 14_Wprowadzenie_do_Redux_Toolkit/02_Implementacja_Nowej_Funkcjonalnosci_z_Redux_Toolkit/js/Redux/actions.jsx
@@ -2,5 +2,20 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const notificationsSlice = createSlice({
   name: 'notifications',
-  initialState: { list: [], loading: false },
+  initialState: { list: [], loading: false, error: null },
+  reducers:{},
+  extraReducers:(builder) => {
+    builder
+      .addCase(.pending, (state, action) => {
+        state.loading = 'loading';
+    });
+    .addCase(.fulfilled, (state, action) => {
+      state.loading = false;
+      state.list.push(action.payload);
+    });
+    .addCase(.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    })
+  }
 });
