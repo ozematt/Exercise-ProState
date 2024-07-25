cd67596d1b8a0eeeeae3e7e01a4f12bb5080cecc
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-25 14:59:20 +0200

    added provider with store

--- 14_Wprowadzenie_do_Redux_Toolkit/02_Implementacja_Nowej_Funkcjonalnosci_z_Redux_Toolkit/js/app.jsx
+++ 14_Wprowadzenie_do_Redux_Toolkit/02_Implementacja_Nowej_Funkcjonalnosci_z_Redux_Toolkit/js/app.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { NotificationsPanel } from './NotificationsPanel';
-const App = () => <NotificationsPanel />;
+
+import { store } from './Redux/store.jsx';
+import { Provider } from 'react-redux';
+
+const App = () => (
+  <Provider store={store}>
+    <NotificationsPanel />
+  </Provider>
+);
 
 const container = document.getElementById('app');
 const root = createRoot(container);
