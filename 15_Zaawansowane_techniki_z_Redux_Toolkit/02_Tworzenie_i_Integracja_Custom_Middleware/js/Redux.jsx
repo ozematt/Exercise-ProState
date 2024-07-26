a8cc8c9b83cbce7e942e308a631357e217d848a3
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-26 16:30:44 +0200

    added reducers

--- 15_Zaawansowane_techniki_z_Redux_Toolkit/02_Tworzenie_i_Integracja_Custom_Middleware/js/Redux.jsx
+++ 15_Zaawansowane_techniki_z_Redux_Toolkit/02_Tworzenie_i_Integracja_Custom_Middleware/js/Redux.jsx
@@ -1,5 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 import { loggerMiddleware } from './loggerMiddleware.js';
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {},
+  reducers: {
+    increment: (state) => state + 1,
+    decrement: (state) => state - 1,
+  },
+});
 
 const store = configureStore({
   reducer: {},
