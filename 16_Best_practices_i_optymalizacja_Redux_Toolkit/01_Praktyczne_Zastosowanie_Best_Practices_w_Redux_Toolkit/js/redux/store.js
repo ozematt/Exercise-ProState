3baccb56e118a30379ed99601822f57970b5decb
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-27 16:09:50 +0200

    added reducers

--- 16_Best_practices_i_optymalizacja_Redux_Toolkit/01_Praktyczne_Zastosowanie_Best_Practices_w_Redux_Toolkit/js/redux/store.js
+++ 16_Best_practices_i_optymalizacja_Redux_Toolkit/01_Praktyczne_Zastosowanie_Best_Practices_w_Redux_Toolkit/js/redux/store.js
@@ -1,8 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 import { createEntityAdapter } from '@reduxjs/toolkit';
 
 const taskAdepter = createEntityAdapter({
   selectId: (task) => task.id,
+});
+
+const taskSlice = createSlice({
+  name: 'tasks',
+  initialState: 0,
+  reducers: {
+    addTask: taskAdepter.addOne,
+    removeTask: taskAdepter.removeOne,
+    updateTask: taskAdepter.updateOne,
+  },
 });
 
 const store = configureStore({});
