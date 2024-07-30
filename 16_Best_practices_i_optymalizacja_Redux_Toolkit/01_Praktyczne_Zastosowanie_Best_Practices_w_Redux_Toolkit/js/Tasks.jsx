77b7f2850965711573999a603e5a059cfa0e0ec2
Mateusz Obirek <neutralnie7@gmail.com>
2024-07-30 10:36:29 +0200

    added comments

--- 16_Best_practices_i_optymalizacja_Redux_Toolkit/01_Praktyczne_Zastosowanie_Best_Practices_w_Redux_Toolkit/js/Tasks.jsx
+++ 16_Best_practices_i_optymalizacja_Redux_Toolkit/01_Praktyczne_Zastosowanie_Best_Practices_w_Redux_Toolkit/js/Tasks.jsx
@@ -3,20 +3,26 @@ import { useSelector, useDispatch } from 'react-redux';
 import { addTask, removeTask, selectAllTasks, selectCompletedTasks, toggleTask } from './redux/tasksSlice.js';
 
 export const Tasks = () => {
+  //state for newTask
   const [newTask, setNewTask] = useState('');
+  //get all tasks
   const tasks = useSelector(selectAllTasks);
+  //get completed tasks
   const completedTasks = useSelector(selectCompletedTasks);
   const dispatch = useDispatch();
 
+  //added new Task
   const handleAddTask = () => {
     if (newTask) {
       dispatch(
+        //dispatch addTask, make structure of task
         addTask({
           id: Math.floor(Math.random() * 1000),
           title: newTask,
           completed: false,
         })
       );
+      //reset state
       setNewTask('');
     }
   };
