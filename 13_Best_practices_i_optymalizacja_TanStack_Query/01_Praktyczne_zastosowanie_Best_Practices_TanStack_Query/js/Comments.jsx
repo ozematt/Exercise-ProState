992ac233370719888300784cb8fa1d093625ccf0
Mateusz Obirek <neutralnie7@gmail.com>
2024-09-20 18:34:40 +0200

    added component comment

--- 13_Best_practices_i_optymalizacja_TanStack_Query/01_Praktyczne_zastosowanie_Best_Practices_TanStack_Query/js/Comments.jsx
+++ 13_Best_practices_i_optymalizacja_TanStack_Query/01_Praktyczne_zastosowanie_Best_Practices_TanStack_Query/js/Comments.jsx
@@ -11,3 +11,39 @@ const addComments = async (comment) => {
     console.error('There has been a problem:', error);
   }
 };
+
+export const Comments = ({ post }) => {
+  //useQueryClient import
+  const queryClient = useQueryClient();
+  //handle form
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: Date.now().toString(),
+      postId: post,
+      body: '',
+    },
+  });
+  //usemutation with invalidation
+  const addCommentsMutation = useMutation({
+    mutationFn: addComments,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['comments'],
+      });
+    },
+  });
+  //on submit add new comment to post with id
+  const onSubmit = (data) => {
+    addCommentsMutation.mutate(data);
+    console.log(data);
+  };
+  //UI
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('body')} type="text" placeholder="Komentarz..." />
+        <button type="submit">Dodaj</button>
+      </form>
+    </>
+  );
+};
