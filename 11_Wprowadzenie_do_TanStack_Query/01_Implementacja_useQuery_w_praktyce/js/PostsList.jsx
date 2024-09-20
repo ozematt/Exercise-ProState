8cafa499f2b3f1601947aa4a1dee69ce46559e72
Mateusz Obirek <neutralnie7@gmail.com>
2024-09-20 18:36:03 +0200

    deleted component

--- 11_Wprowadzenie_do_TanStack_Query/01_Implementacja_useQuery_w_praktyce/js/PostsList.jsx
+++ 11_Wprowadzenie_do_TanStack_Query/01_Implementacja_useQuery_w_praktyce/js/PostsList.jsx
@@ -5,22 +5,3 @@ const fetchData = async () => {
   const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
   return data;
 };
-
-export const PostsList = () => {
-  const { isPending, data, isError, error } = useQuery({ queryKey: ['posts'], queryFn: fetchData });
-
-  return (
-    <>
-      {isPending && <div>Loading...</div>}
-      {isError && <div>{error.message}</div>}
-      <ol>
-        {data.map((post) => (
-          <li key={post.id}>
-            <h6>{post.title}</h6>
-            <p>{post.body.slice(0, 100)}...</p>
-          </li>
-        ))}
-      </ol>
-    </>
-  );
-};
