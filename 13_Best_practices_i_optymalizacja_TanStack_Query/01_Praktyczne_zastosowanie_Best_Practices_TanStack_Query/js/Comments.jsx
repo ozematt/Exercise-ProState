340e0975c4b091386a7b564b7b258245490fbcf0
Mateusz Obirek <neutralnie7@gmail.com>
2024-09-20 18:35:24 +0200

    added new imports

--- 13_Best_practices_i_optymalizacja_TanStack_Query/01_Praktyczne_zastosowanie_Best_Practices_TanStack_Query/js/Comments.jsx
+++ 13_Best_practices_i_optymalizacja_TanStack_Query/01_Praktyczne_zastosowanie_Best_Practices_TanStack_Query/js/Comments.jsx
@@ -1,3 +1,7 @@
+import { useForm } from 'react-hook-form';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+
 //add comments fn
 const addComments = async (comment) => {
   try {
