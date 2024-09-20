a68c85f959d08fb96edb512bb803b6bd5d7b8cb4
Mateusz Obirek <neutralnie7@gmail.com>
2024-09-20 18:33:57 +0200

    added comment fetch

--- 13_Best_practices_i_optymalizacja_TanStack_Query/01_Praktyczne_zastosowanie_Best_Practices_TanStack_Query/js/Comments.jsx
+++ 13_Best_practices_i_optymalizacja_TanStack_Query/01_Praktyczne_zastosowanie_Best_Practices_TanStack_Query/js/Comments.jsx
@@ -3,6 +3,14 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
 //add comments fn
+const addComments = async (comment) => {
+  try {
+    const { data: result } = await axios.post('http://localhost:3001/comments', comment);
+    console.log(result);
+  } catch (error) {
+    console.error('There has been a problem:', error);
+  }
+};
 
 export const Comments = ({ post }) => {
   //useQueryClient import
